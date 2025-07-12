106d495089b3c2281808e00dbd2553fcf42d9365
hackingbandy <hackingbandy@gmail.com>
2025-07-12 17:11:54 +0200

    start rag fa

--- financial_advisor.py
+++ financial_advisor.py
@@ -1,1 +1,10 @@
-#build FA
+from rag import RAG
+from typing import Dict, List
+
+
+
+class FinancialAdvisor(RAG):
+    def __init__(self):
+        super().__init__()
+        print("Financial Advisor initialisiert.")
+FinancialAdvisor()
